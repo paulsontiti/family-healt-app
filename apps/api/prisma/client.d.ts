a9b0465ae96547ddf314fe9e06ca8f9c5584ce5e
Nwoha Paulson <paulsontiti@gmail.com>
2026-03-23 13:05:21 +0100

    preinstall hook

--- apps/api/prisma/client.d.ts
+++ apps/api/prisma/client.d.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
-declare const prisma: import("../generated/prisma/internal/class").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace").GlobalOmitConfig | undefined, import("@prisma/client/runtime/library").DefaultArgs>;
+import { PrismaClient } from "@prisma/client";
+declare const prisma: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
 export default prisma;
 //# sourceMappingURL=client.d.ts.map

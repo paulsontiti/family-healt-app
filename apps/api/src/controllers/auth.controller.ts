c2eee525ea0a3647f8e10f777daca06801195eee
Nwoha Paulson <paulsontiti@gmail.com>
2026-03-25 19:19:44 +0100

    mvp - register setParent

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -3,7 +3,11 @@ import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import prisma from "../../prisma/client";
 import { hashPassword, comparePasswords } from "../utils/bcrypt.utils";
-import { createParent, getParent, getParentEmail } from "../services/auth.service";
+import {
+  createParent,
+  getParent,
+  getParentEmail,
+} from "../services/auth.service";
 
 export const authController = {
   register: async (req: Request, res: Response) => {
@@ -11,15 +15,24 @@ export const authController = {
       const { name, email, password } = req.body;
       const hashedPassword = await hashPassword(password);
 
-      const user = await createParent({
+      const parent = await createParent({
         name,
         email,
         password: hashedPassword,
       });
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      const token = jwt.sign({ parentId: parent.id }, process.env.JWT_SECRET!, {
         expiresIn: "7d",
       });
-      res.status(201).json({ user: { id: user.id, email: user.email }, token });
+
+      res.status(201).json({
+        parent: {
+          id: parent.id,
+          email: parent.email,
+          familyId: parent.familyId,
+          name: parent.name,
+        },
+        token,
+      });
     } catch (err: any) {
       res.status(500).json({ message: err.message });
     }
@@ -49,7 +62,7 @@ export const authController = {
         token,
       });
     } catch (err: any) {
-      console.log(err)
+      console.log(err);
       res.status(500).json({ message: err.message });
     }
   },
